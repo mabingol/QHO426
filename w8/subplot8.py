900d58009d09c7b5b019dddafc3bad8b6c49b5b1
Muhammed-AliA30 <>
2022-05-07 10:59:29 +0000

    after animation

--- w8/subplot8.py
+++ w8/subplot8.py
@@ -7,7 +7,7 @@ y = np.array([3, 8, 1, 10])
 
 plt.subplot(1, 2)
 plt.plot(x,y)
-#plt.title("Expenses")
+plt.title("Expenses")
 
 #plot 2:
 x = np.array([0, 1, 2, 3])
@@ -15,7 +15,7 @@ y = np.array([10, 20, 30, 40])
 
 plt.subplot(1, 2, 2)
 plt.plot(x,y)
-#plt.title("Income")
+plt.title("Income")
 
 plt.title("MY BUSSINESS")
 plt.show()
---
+++ w9/anim2.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+fig, ax = plt.subplots()
+
+def animate(frame):
+  global ax
+  ax.plot(range(range), range(frame))
+
+squares_animation = animation.FuncAnimation(fig, animate, frames = 10, interval = 100)
+
+plt.show()
